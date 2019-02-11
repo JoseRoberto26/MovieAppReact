c6f60bb9a64592de3cf756f7f6f080188d4f8aeb
JR-PC\JR <jr.lsf26@gmail.com>
2019-02-11 00:20:20 -0200

    Ajustes no movie-item, tratamento dos gêneros do filme e limite de resultados por página

--- movieapp/src/home-component/home.js
+++ movieapp/src/home-component/home.js
@@ -2,17 +2,27 @@ import React, { Component } from 'react';
 import MovieItem from '../movie-item/MovieItem.js';
 var axios = require('axios');
 const apiKey = '5a240d5ac38592ee034f80a46ddeadbd';
+const pageLimit = 5;
 
 export default class Home extends Component {
   constructor(props){
     super(props);
-    this.state = {items: []};
+    this.state = {items: [], genres: [], currentPage: 1};
+  }
+
+  componentWillMount(){
+      var genreURI = window.encodeURI('https://api.themoviedb.org/3/genre/movie/list?api_key='+apiKey+'&language=pt-BR')
+
+      axios.get(genreURI).then(response => {
+          this.state.genres = response.data.genres;
+      })
+
   }
 
   componentDidMount(){
       var encodedURI = window.encodeURI('https://api.themoviedb.org/3/movie/popular?api_key='+apiKey+'&language=pt-BR');
 
-      return axios.get(encodedURI).then(response =>{
+      axios.get(encodedURI).then(response =>{
           this.setState({items : response.data.results});
       })
   }
@@ -35,8 +45,24 @@ export default class Home extends Component {
   render() {
       let renderItems;
       const items = this.state.items;
-      renderItems = items.map((item) => {
-          return <MovieItem key ={item.id} movie ={item}/>;
+      const genres = this.state.genres;
+
+      const lastIndex = this.state.currentPage * pageLimit;
+      const firstIndex = lastIndex - pageLimit;
+
+      const currentItems = items.slice(firstIndex, lastIndex);
+
+      renderItems = currentItems.map((item) => {
+          let itemGenreNames = [];
+          item.genre_ids.forEach(itemGenre => {
+              console.log(item);
+              genres.forEach(genre => {
+                  if(genre.id == itemGenre){
+                      itemGenreNames.push(genre.name);
+                  }
+              })
+          })
+          return <MovieItem key ={item.id} movie ={item} genres={itemGenreNames}/>;
       })
 
 
@@ -46,6 +72,7 @@ export default class Home extends Component {
 
         <input type="text" onChange={this.searchHandler.bind(this)} className={"searchInput"} placeholder={"Digite"}/>
           {renderItems}
+
 
 
       </div>
--- movieapp/src/movie-item/MovieItem.js
+++ movieapp/src/movie-item/MovieItem.js
@@ -1,5 +1,8 @@
 import React, { Component } from 'react';
+import Moment from 'react-moment';
 import { Redirect } from 'react-router-dom'
+
+const imgBaseUrl = "http://image.tmdb.org/t/p/w185";
 
 
 export default class MovieItem extends Component {
@@ -21,11 +24,32 @@ export default class MovieItem extends Component {
         }
     }
 
+    formatScore(number){
+        return number *10 + "%";
+    }
+
+
+    imagePath(string){
+        if(string) {
+            return imgBaseUrl + string;
+        }
+    else{
+            return "../assets/not-found.png"
+        }
+    }
+
     render() {
     return  (
       <div className="movieitem" onClick={this.setRedirect}>
           {this.renderRedirect()}
           <h1>{this.props.movie.title}</h1>
+          <p> {this.props.genres} </p>
+          <Moment format="DD/MM/YYYY">
+              {this.props.movie.release_date}
+          </Moment>
+          <p>{this.formatScore(this.props.movie.vote_average)}</p>
+          <p>{this.props.movie.overview}</p>
+          <img src={this.imagePath(this.props.movie.poster_path)}/>
 
       </div>
     )
